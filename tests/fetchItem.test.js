10e018d1a3d43278461a0bfd2fd92b569f4963af
Mariana Saraiva <marianascm8@gmail.com>
2021-10-21 13:03:22 -0300

    Finalizo parte de testes requisito 2

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -6,5 +6,21 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('1- Verifica se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toEqual('function');
+  });
+  it('2 - Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('3 - Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527"', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+  it('4 - Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item); 
+  });
+  it('5 - Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: You must provide an url.', async () => {
+    await expect(fetchItem()).rejects.toEqual(new Error('You must provide an url'));
+  });
 });
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,12 +4,12 @@ const computadorSearch = require('../mocks/search');
 
 window.fetch = jest.fn(fetchSimulator);
 
-describe('1 - Teste a função fecthProducts', () => {
+describe('1. Teste a função fecthProducts', () => {
   // implemente seus testes aqui
   it('1- Verifica se fetchProducts é uma função', () => {
     expect(typeof fetchProducts).toEqual('function');
   });
-  it('2. Testa se a função com argumento e se fetch foi chamada', async () => {
+  it('2. Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async () => {
     expect.assertions(1);
     await fetchProducts('computador');
     expect(fetch).toHaveBeenCalled(); // testado com not deu erro.
